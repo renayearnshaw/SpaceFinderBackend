f319e988733f84881b3a932c213175cbbcabe34f
Renay Earnshaw <renayearnshaw@gmail.com>
2022-10-26 15:33:50 +0100

    Link lambda with API Gateway

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -6,8 +6,11 @@ import {
   Runtime,
 } from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 
 export class SpaceStack extends Stack {
+  private api = new RestApi(this, "SpaceApi");
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
@@ -16,5 +19,9 @@ export class SpaceStack extends Stack {
       code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
       handler: "hello.main",
     });
+
+    const lambdaIntegration = new LambdaIntegration(helloLambda);
+    const lambdaResource = this.api.root.addResource("hello");
+    lambdaResource.addMethod("GET", lambdaIntegration);
   }
 }
